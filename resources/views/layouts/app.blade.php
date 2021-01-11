f3b81761f5abde4ef0ddf41a6989d190691af027
Martynas <martynasssk@gmail.com>
2021-01-11 21:46:28 +0200

    connecting db, starting migrating, adding field, roolback, controlers using, registration form kreting, using Tailwind styles

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,8 +8,8 @@
 
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
-<body class="bg-gray-100">
-    <nav class="p-6 bg-white flex justify-between" >
+ <body class="bg-gray-200"> {{--  spalva backgroundo  --}}
+    <nav class="p-6 bg-white flex justify-between mb-6" >
         <ul class="flex items-center">
             <li>
                 <a href="" class="p-3">Home</a>
@@ -30,10 +30,10 @@
                 <a href="" class="p-3">Login</a>
             </li>
             <li>
-                <a href="" class="p-3">Register</a>
+                <a href="{{ route('register') }}" class="p-3">Register</a>
             </li>
             <li>
-                <a href="" class="p-3">Logout</a>
+                <a href="/register" class="p-3">Logout</a>
             </li>
         </ul>
     </nav>
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,12 @@
 <?php
 
+use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/register', [RegisterController::class, 'index'])->name('register');
 
 
 Route::get('/posts', function () {
     return view('posts.index');
 });
+
